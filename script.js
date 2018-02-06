4cea74ad3c5f5bc08f03dfc86337959cc295684b
Michael Gregoire <mdgregoire@gmail.com>
2018-02-06 11:32:31 -0600

    added student array constructor

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-console.log('js');
 $(document).ready(onReady);
+let students=[];
+class StudentConstructor {
+  constructor (userName, actualName){
+    this.userName = userName;
+    this.actualName = actualName;
+  }
+}
+
+
+
+
 
 function onReady(){
-console.log('JQ');  
+
+
+// console.log(Math.floor(Math.random() * (1 + max - min) + min));
+      // return Math.floor(Math.random() * (1 + max - min) + min);
+
+students.push(new StudentConstructor('mdgregoire', 'Michael'));
+students.push(new StudentConstructor('crouton21', 'Christiana'));
+students.push(new StudentConstructor('alex-aspell', 'Alex'));
+students.push(new StudentConstructor('tenzin1993', 'Tenzin'));
+
+console.log(students);
 }
