fff9ed56032c77a6d60000b91d34cd666691d0fa
Michael Gregoire <mdgregoire@gmail.com>
2018-02-06 12:02:16 -0600

    lunch break

--- script.js
+++ script.js
@@ -7,23 +7,24 @@ class StudentConstructor {
   }
 }
 let randomNumber;
-
-
-
-
 function onReady(){
-
 students.push(new StudentConstructor('mdgregoire', 'Michael'));
 students.push(new StudentConstructor('crouton21', 'Christiana'));
 students.push(new StudentConstructor('alex-aspell', 'Alex'));
 students.push(new StudentConstructor('tenzin1993', 'Tenzin'));
-
 randomNumber = Math.floor(Math.random()*students.length);
-console.log(randomNumber);
-console.log(students[randomNumber]);
-
+// console.log(randomNumber);
+// console.log(students[randomNumber]);
 $('#randomName').text(students[randomNumber].actualName);
 
 
 
+for(i=0; i<students.length; i++){
+console.log('in for loop');
+$('#generatedImages').append(`<img src ='https://github.com/${students[i].userName}.png' data-user=(${students[i].actualName})>`);
+
 }
+
+
+
+}//end onready
