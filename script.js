c387baccfcffb7cad0b14a6628771283c74b61b1
Michael Gregoire <mdgregoire@gmail.com>
2018-02-06 12:36:09 -0600

    added comments

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 $(document).ready(onReady);
-let students=[];
+let students=[]; //global array to store student information
+let randomNumber; //global variable to store random number
 class StudentConstructor {
   constructor (userName, actualName){
     this.userName = userName;
     this.actualName = actualName;
   }
-}
-let randomNumber;
+} //end student constructor
+
 function onReady(){
-students.push(new StudentConstructor('mdgregoire', 'Michael'));
-students.push(new StudentConstructor('crouton21', 'Christiana'));
-students.push(new StudentConstructor('alex-aspell', 'Alex'));
-students.push(new StudentConstructor('tenzin1993', 'Tenzin'));
-randomNumber = Math.floor(Math.random()*students.length);
-// console.log(randomNumber);
-// console.log(students[randomNumber]);
-$('#randomName').text(students[randomNumber].actualName);
+  students.push(new StudentConstructor('mdgregoire', 'Michael'));
+  students.push(new StudentConstructor('crouton21', 'Christiana'));
+  students.push(new StudentConstructor('alex-aspell', 'Alex'));
+  students.push(new StudentConstructor('tenzin1993', 'Tenzin'));
+  randomNumber = Math.floor(Math.random()*students.length);//ranadom number generator
+  $('#randomName').text(students[randomNumber].actualName);//puts random name on the dom
 
 
 
-for(i=0; i<students.length; i++){
-console.log('in for loop');
-$('#generatedImages').append(`<img src ='https://github.com/${students[i].userName}.png' data-user=(${students[i].actualName})>`);
+  for(i=0; i<students.length; i++){
+    console.log('in for loop');
+    $('#generatedImages').append(`<img src ='https://github.com/${students[i].userName}.png' class = 'studentImage' data-user=(${students[i].actualName})>`);
+  }// end for loop to generate Images
 
-}
 
 
 
